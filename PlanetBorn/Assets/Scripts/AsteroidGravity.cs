9fc21f41b66d507701cef45e6542736577bd310d
Juan SB <juanyo.sb@gmail.com>
2021-04-16 21:06:55 +0200

    updated gravity pero no funciona

--- PlanetBorn/Assets/Scripts/AsteroidGravity.cs
+++ PlanetBorn/Assets/Scripts/AsteroidGravity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class AsteroidGravity : MonoBehaviour
 {
+    public float fuerza;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +17,13 @@ public class AsteroidGravity : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D c)
+    private void OnTriggerStay2D(Collider2D c)
     {
         if (c.gameObject.GetComponent<Asteroid>())
         {
             Debug.Log("Asteroid entered" + c);
             //Esto no funciona lol
+            c.gameObject.GetComponent<Rigidbody2D>().AddForce(fuerza*c.transform.position - this.transform.position);
             //c.transform.position = c.transform.position - asteroid.transform.position;
         }
     }
